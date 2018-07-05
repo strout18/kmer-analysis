4e15720b339165cc9c4da00d5fac142293ca8d4c
strout18 <40800240+strout18@users.noreply.github.com>
2018-07-05 15:47:27 -0500

    Update NucDiversity.py

--- KmerMetrics/NucDiversity.py
+++ KmerMetrics/NucDiversity.py
@@ -2,17 +2,20 @@
 #downreg genes)
 def diversity(udiffsorted, ddiffsorted):
   count = 0
-  for x in upregdiffsorted:
-      if len(set(x[0])) > 2:
+  for x in udiffsorted:
+      if len(set(x[0])) > 2: #if set of unique nucs in k-mer > 2
           count += 1
-  print (count, 'diverse k-mers out of', len(upregdiffsorted), 'that comprise a greater percent of all kmers in upregulated genes.')
-  print ("%s / %s = %s" % (count, len(upregdiffsorted), count/ len(upregdiffsorted)))
-  count = 0
-  for x in downregdiffsorted:
-      if len(set(x[0])) > 2:
-          count += 1
-  print (count, 'diverse k-mers out of', len(downregdiffsorted), 'that comprise a greater percent of all kmers in downregulated genes.')
+  print (count, 'diverse k-mers out of', len(udiffsorted), 'that comprise a greater percent of all kmers in upregulated genes.')
   try:
-      print ("%s / %s = %s" % (count, len(downregdiffsorted), count/ len(downregdiffsorted)))  
+    print ("%s / %s = %s" % (count, len(udiffsorted), count/ len(udiffsorted))) #return # diverse kmers/ all kmers
+  except ZeroDivisionError:
+    pass
+  count = 0
+  for x in ddiffsorted:
+      if len(set(x[0])) > 2:
+          count += 1
+  print (count, 'diverse k-mers out of', len(ddiffsorted), 'that comprise a greater percent of all kmers in downregulated genes.')
+  try:
+      print ("%s / %s = %s" % (count, len(ddiffsorted), count/ len(ddiffsorted)))  
   except ZeroDivisionError:
       pass
