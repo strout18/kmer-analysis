5cb9843fa9f47a742ef1b8e4303481b9bc04d6a1
strout18 <40800240+strout18@users.noreply.github.com>
2018-07-05 16:15:47 -0500

    Update nuccontent.py

--- KmerMetrics/nuccontent.py
+++ KmerMetrics/nuccontent.py
@@ -1,23 +1,22 @@
-def nuc_content(sortedlst, cutoff, nuc): #dct = upregdiff or downregdiff, nuc = 'A', 'C' ,'T', 'G' 
-		#UPREG SHOULD BE SORTED GREATEST TO LEAST, DOWNREG LEAST TO GREATEST
-				nuc = nuc.upper()
-				nuccount = []
-				for i in range(cutoff):
-						count = 0
-						for x in sortedlst[i][0]:
-								if x.upper() == nuc:
-										count +=1
-						nuccount.append(count)
-				avg = sum(nuccount) / len(nuccount)
-				print ('Average number of %s in %s-mer: %s' %(nuc, kmerlength, avg))
-numcutoff = 200
-if len(upregdiffsorted) < 200 or len(downregdiffsorted) < 200:
-		if len(upregdiffsorted) > len(downregdiffsorted):
-				numcutoff = len(downregdiffsorted)
+#params: udiffsorted (list of sorted tuples from BasicAnalysis.py for upreg genes), ddiffsorted (same for downreg genes) numcutoff (# of 
+#k-mers from most frequent to cutoff to analyze nuc_content for), nuc ('A', 'C', 'T', or 'G')
+def nuc_content(udiffsorted, ddiffsorted, nuc, numcutoff = 200): 
+	nuc = nuc.upper()
+	nuccount = []
+	if len(udiffsorted) < 200 or len(ddiffsorted) < 200:
+		#if either list has fewer k-mers than cutoff, it goes with lowest # as new cutoff for both lists
+		if len(udiffsorted) > len(ddiffsorted):
+			cutoff = len(ddiffsorted)
 		else:
-				numcutoff = len(upregdiffsorted)
-print ('Cutoff:', numcutoff)
-print ('Upregulated')
-nuc_content(upregdiffsorted, numcutoff, nuc)
-print ('Downregulated')
-nuc_content(downregdiffsorted, numcutoff, nuc)
+			cutoff = len(udiffsorted)
+	else:
+		cutoff = numcutoff
+	for i in range(cutoff):
+	    count = 0
+	    for x in sortedlst[i][0]:
+		if x.upper() == nuc:
+		    count +=1
+	    nuccount.append(count)
+	avg = sum(nuccount) / len(nuccount)
+	print ('Cutoff:', cutoff) 
+	print ('Average number of %s in %s-mer: %s' %(nuc, kmerlength, avg))
