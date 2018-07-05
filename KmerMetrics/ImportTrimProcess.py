9d57428ec102577705821e5c1b7feb68fb6041d9
strout18 <40800240+strout18@users.noreply.github.com>
2018-07-05 16:40:11 -0500

    Update ImportTrimProcess.py

--- KmerMetrics/ImportTrimProcess.py
+++ KmerMetrics/ImportTrimProcess.py
@@ -1,19 +1,22 @@
-peakfasta = "/storage/cylin/grail/projects/stella_summer/currentFiles/enhancersonly.fa"
-rtable = "/storage/cylin/grail/projects/stella_summer/currentFiles/R_EnhancersFiltered.txt"
-#TO DO: ADD IF FREQ2 IN OPPDICT KEYS CLAUSE TO ALL LENGTH KMERS
-import csv
-gene2enhancer = {}
-enhancer2gene = {}
-gene_fc = {}
+peakfasta = "" #PUT YOUR FASTA FILE HERE
+rtable = "" #PUT YOUR DATA TABLE HERE (SEE DATAPROCESSING.R)
+import csv 
+gene2enhancer = {} #dict in form geneID: chr+start+stop
+enhancer2gene = {} 
+gene_fc = {} #dict in form geneID: fold change
 with open(rtable) as f:
     data = csv.reader(f, delimiter = "\t")
     for row in data:
         if row[0] != "geneid":
             i = 0
+            #the following code is put in place in case you have multiple sequences assigned to the same gene. It adds 1 to a number 
+            #in front of the gene ID until a unique gene ID is generated so you can keep your sequences separate
+            #for example: the first time the program sees the id "VCAM1" it assigns that particular sequence the gene ID "0VCAM1", 
+            #the 2nd time the current sequence is assigned "1VCAM1", etc.
             nonunique = True
-            while nonunique:
-                geneid = str(i) + row[0]
-                if geneid not in gene2enhancer.keys():
+            while nonunique: #loops through until it generates a unique gene ID
+                geneid = str(i) + row[0] 
+                if geneid not in gene2enhancer.keys(): 
                     nonunique = False
                 i += 1
             gene2enhancer[geneid] = row[1] + ":" + str(row[2]) + "-" + str(row[3])
@@ -21,19 +24,19 @@ with open(rtable) as f:
             gene_fc[geneid] = str(row[4])
     f.close()
 j = 0
-for key in gene2enhancer.keys():
+for key in gene2enhancer.keys(): #optional to get a sense of the output
     print (key, gene2enhancer[key])
-    if j>10:
+    if j>10: 
         break
     j += 1
 j = 0
-for key in enhancer2gene.keys():
+for key in enhancer2gene.keys(): #optional to get a sense of the output
     print (key, enhancer2gene[key])
     if j>10:
         break
     j += 1
-gene_seq = {}
-with open(peakfasta) as f:
+gene_seq = {} #dict in form geneID, sequence
+with open(peakfasta) as f: #trims FASTA
     for line in f:
         if line[0] == ">":
             chromid = line[1:len(line)-1] #removes newline char
@@ -41,7 +44,7 @@ with open(peakfasta) as f:
                 gene_seq[enhancer2gene[chromid]] = next(f).upper()
     f.close()
 i = 0
-for key in gene_seq:
+for key in gene_seq: #optional to get a sense of the output
     print (key, gene_seq[key])
     i += 1
     if i>10:
