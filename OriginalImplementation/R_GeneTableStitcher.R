bc9fd09ccccecf7656a9f65e985d6f626f082682
strout18 <40800240+strout18@users.noreply.github.com>
2018-07-06 11:22:40 -0500

    Update R_GeneTableStitcher.R

--- OriginalImplementation/R_GeneTableStitcher.R
+++ OriginalImplementation/R_GeneTableStitcher.R
@@ -1,4 +1,4 @@
-fpkm = read.table("/Users/stella/Desktop/Genes.fpkm_table.txt")
+fpkm = read.table("/xxxxx/Genes.fpkm_table.txt")
 rownames(fpkm) = fpkm[,1]
 fpkm = fpkm[,-1]
 colnames(fpkm) = c("0hr_rep0", "0hr_rep1", "4hr_rep0", "4hr_rep1")
@@ -15,12 +15,11 @@ fpkm$testavg[fpkm$`4hr_rep0`==0 | fpkm$`4hr_rep1`==0] = fpkm$`4hr_rep1`[fpkm$`4h
 fpkm$foldchange = 0
 fpkm$foldchange[fpkm$ctrlavg != 0 & fpkm$testavg !=0] = log2(fpkm$testavg[fpkm$ctrlavg!=0 & fpkm$testavg != 0]/fpkm$ctrlavg[fpkm$ctrlavg!=0 & fpkm$testavg!=0])
 library(readxl)
-e2g = read_xlsx("/Users/stella/Desktop/Lin Lab/HAEC_P65_TNF_4hr_1_peaks_AllEnhancers_ENHANCER_TO_GENE.xlsx")
+e2g = read_xlsx("/xxxxxx/HAEC_P65_TNF_4hr_1_peaks_AllEnhancers_ENHANCER_TO_GENE.xlsx")
 fpkm$geneid = rownames(fpkm)
 final = merge(fpkm, e2g, by.x = "geneid", by.y = "CLOSEST_GENE")
 final = subset(final, select = c("geneid", "foldchange", "CHROM", "START", "STOP"))
 final = final[,c(1,3,4,5,2)]
-#write.table(final, file = "/Users/stella/Desktop/Lin Lab/R_FinalOut.txt", quote = FALSE, sep = "\t", row.names = FALSE)
 max10 = fpkm[,c(1:4)]
 max10$max = apply(max10, 1, max)
 max10 = max10[max10$max > 10, ]
@@ -30,4 +29,4 @@ max10final = max10final[,c(1:5)]
 downreg = max10final[,c(1, 6, 7)]
 downreg = downreg[downreg$foldchange < 0, ]
 max10final = max10final[abs(max10final$foldchange) > 1, ]
-write.table(max10final, file = "/Users/stella/Desktop/Lin Lab/R_FinalFiltered.txt", quote = FALSE, sep = "\t", row.names = FALSE)
+write.table(max10final, file = "/xxxxxx/R_FinalFiltered.txt", quote = FALSE, sep = "\t", row.names = FALSE)
