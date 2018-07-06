4d33b4bb0af145e1fdf81a1450843ae82996fbd6
strout18 <40800240+strout18@users.noreply.github.com>
2018-07-06 11:12:21 -0500

    Update and rename enhancersonlybed.R to Enhancersonlybed.R

--- OriginalImplementation/Enhancersonlybed.R
+++ OriginalImplementation/Enhancersonlybed.R
@@ -1,11 +1,11 @@
-fasta = read.table("/Users/stella/Downloads/enhanceristhisright.bed")
+fasta = read.table("/xxxxxx/enhanceristhisright.bed")
 colnames(fasta) = c("chr", "start", "stop")
 fasta = fasta[,1:3]
 fasta$all = paste(fasta$chr, fasta$start, fasta$stop, sep = " ")
-max10 = read.table("/Users/stella/Desktop/Lin Lab/R_FinalFiltered.txt")
+max10 = read.table("/xxxxx/R_FinalFiltered.txt")
 colnames(max10) = c("geneid", "CHROM", "START", "STOP", "foldchange")
 max10 = max10[-1,]
 max10$all = paste(max10$CHROM, max10$START, max10$STOP, sep = " ")
 ultimate = merge(fasta, max10, by.x = "all", by.y = "all")
 ultimate = ultimate[, c("geneid", "CHROM", "START", "STOP", "foldchange")]
-write.table(ultimate, file = "/Users/stella/Desktop/Lin Lab/R_EnhancersFiltered.txt", quote = FALSE, sep = "\t", row.names = FALSE)
+write.table(ultimate, file = "/xxxxxx/R_EnhancersFiltered.txt", quote = FALSE, sep = "\t", row.names = FALSE)
