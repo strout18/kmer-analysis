ce0276264de149b1e91f59b05619702cf20d4f4f
strout18 <40800240+strout18@users.noreply.github.com>
2018-07-06 11:11:54 -0500

    Create Promotersonlybed.R

---
+++ OriginalImplementation/Promotersonlybed.R
@@ -0,0 +1,11 @@
+fasta = read.table("xxxxx/promoteristhisright.bed")
+colnames(fasta) = c("chr", "start", "stop")
+fasta = fasta[,1:3]
+fasta$all = paste(fasta$chr, fasta$start, fasta$stop, sep = " ")
+max10 = read.table("/xxxxxx/R_FinalFiltered.txt")
+colnames(max10) = c("geneid", "CHROM", "START", "STOP", "foldchange")
+max10 = max10[-1,]
+max10$all = paste(max10$CHROM, max10$START, max10$STOP, sep = " ")
+ultimate = merge(fasta, max10, by.x = "all", by.y = "all")
+ultimate = ultimate[, c("geneid", "CHROM", "START", "STOP", "foldchange")]
+write.table(ultimate, file = "/xxxxxx/R_PromotersFiltered.txt", quote = FALSE, sep = "\t", row.names = FALSE)
